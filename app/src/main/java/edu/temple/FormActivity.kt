fc29b97dc24a079bde7f74920bb956dedf514053
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-09-22 20:19:09 -0400

    created listeners to EditText files

--- app/src/main/java/edu/temple/FormActivity.kt
+++ app/src/main/java/edu/temple/FormActivity.kt
@@ -2,10 +2,30 @@ package edu.temple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val address = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val saveEmail = address.text.toString()
+
+        val firstPass = findViewById<EditText>(R.id.editTextTextPassword)
+        val savePW = firstPass.text.toString()
+
+        val confirmPass = findViewById<EditText>(R.id.editTextTextPassword2)
+        val confirmPW = confirmPass.text.toString()
+
+
+
+
+
     }
 }
